e08244dbada8c6dd809bf3eeb05aa0a5e18d4ad0
Emerson <emerson.day@gmail.com>
2025-11-15 14:22:00 -0800

    use auth

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: `${import.meta.env.VITE_COGNITO_AUTHORITY}`,
+  client_id: `${import.meta.env.VITE_COGNITO_CLIENT_ID}`,
+  redirect_uri: "http://localhost:5173/",
+  response_type: "code",
+  scope: "phone openid email",
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
